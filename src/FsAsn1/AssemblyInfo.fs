ab79eedfbfcf54dcb9ac8bdebd35f6c774411635
i-p <ivan12@gmail.com>
2016-07-21 22:29:00 +0200

    Initial commit

---
+++ src/FsAsn1/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsAsn1")>]
+[<assembly: AssemblyProductAttribute("FsAsn1")>]
+[<assembly: AssemblyDescriptionAttribute("An F# library for reading ASN.1 encoded data.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
