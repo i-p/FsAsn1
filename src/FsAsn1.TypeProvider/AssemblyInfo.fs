223d7b908897e4ce3fc926e48a3d951993de869e
i-p <ivan12@gmail.com>
2016-08-03 20:15:26 +0200

    Add type provider project

---
+++ src/FsAsn1.TypeProvider/AssemblyInfo.fs
@@ -0,0 +1,11 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsAsn1.TypeProvider")>]
+[<assembly: AssemblyProductAttribute("FsAsn1.TypeProvider")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
