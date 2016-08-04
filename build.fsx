268cb620361afdc46c0a2a7c4c92b0a5c2b60e9e
i-p <ivan12@gmail.com>
2016-08-04 20:50:50 +0200

    Add example test for type provider

--- build.fsx
+++ build.fsx
@@ -20,11 +20,26 @@ Target "Build" (fun _ ->
 )
 
 Target "RunTests" (fun _ ->
-    !! (buildDir @@ "FsAsn1.*.dll")
+    !! (buildDir @@ "FsAsn1.Tests.dll")
     |> NUnit3 (fun p -> 
         { p with
             TimeOut = TimeSpan.FromMinutes 5. })
 )
+
+Target "RunTPTests" (fun _ ->
+
+    let tpAssemblies = """
+#I __SOURCE_DIRECTORY__
+#r @"..\..\src\FsAsn1.TypeProvider\bin\Release\FsAsn1.dll"
+#r @"..\..\src\FsAsn1.TypeProvider\bin\Release\FsAsn1.TypeProvider.dll"
+"""
+    //TODO restore previous content
+    System.IO.File.WriteAllText("tests/FsAsn1.Tests/load-tp-assemblies.fsx", tpAssemblies) 
+
+    !! "tests/FsAsn1.Tests/Script1.fsx"
+    |> Seq.iter (fun f -> executeFSI "./" f Seq.empty |> snd |> Seq.iter (fun msg -> if msg.IsError then traceError msg.Message else trace msg.Message))
+)
+
 
 Target "All" DoNothing
 
---
+++ tests/FsAsn1.Tests/Script1.fsx
@@ -0,0 +1,54 @@
+﻿#r "../../packages/NUnit/lib/net45/nunit.framework.dll"
+#r "../../packages/FParsec-Big-Data-Edition/lib/net45/FParsecCS.dll"
+#r "../../packages/FParsec-Big-Data-Edition/lib/net45/FParsec.dll"
+
+#load "load-tp-assemblies.fsx"
+
+[<Literal>]
+let ss = @"
+    Certificate  ::=  SEQUENCE  {
+        tbsCertificate       TBSCertificate,
+        signatureAlgorithm   AlgorithmIdentifier,
+        signatureValue       BIT STRING  }
+
+   TBSCertificate  ::=  SEQUENCE  {
+        version         [0]  EXPLICIT Version DEFAULT v1,
+        serialNumber         CertificateSerialNumber,
+        signature            AlgorithmIdentifier,
+        issuer               Name,
+        validity             Validity,
+        subject              Name,
+        subjectPublicKeyInfo SubjectPublicKeyInfo,
+        issuerUniqueID  [1]  IMPLICIT UniqueIdentifier OPTIONAL,
+        subjectUniqueID [2]  IMPLICIT UniqueIdentifier OPTIONAL,
+        extensions      [3]  EXPLICIT Extensions OPTIONAL
+        }
+
+    Version  ::=  INTEGER  {  v1(0), v2(1), v3(2)  }
+
+    CertificateSerialNumber  ::=  INTEGER
+        "
+
+open System.IO
+open FsAsn1.Reader
+open NUnit.Framework
+
+
+let s = FsAsn1.SchemaParser.parseTypeAssignments ss |> dict
+
+let ctx = AsnContext(AsnArrayStream(File.ReadAllBytes(__SOURCE_DIRECTORY__ + "\Data\google_ssl.cer"), 0), (fun str -> if s.ContainsKey str then Some s.[str] else None ))
+let element = readElement ctx (Some s.["Certificate"]) 
+
+type F = FsAsn1.Provided.AsnProvider<ss>
+
+let c = F.Certificate.fromElement(element)
+
+c.signatureAlgorithm
+c.signatureValue
+
+Assert.AreEqual(bigint 2, c.tbsCertificate.version)
+
+
+
+
+
