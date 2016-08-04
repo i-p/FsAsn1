5f78b80d8fe03a56f8064a312596882416ea1dbb
i-p <ivan12@gmail.com>
2016-08-04 20:43:47 +0200

    Add PowerShell build script for type provider

---
+++ Build-TypeProvider.ps1
@@ -0,0 +1,50 @@
+﻿function Set-UniqueAssemblyName {
+    param($projectName)
+
+    $proj = Get-Project $projectName
+    $prop = $proj.Properties.Item("AssemblyName")
+    $originalAssemblyName = $prop.Value
+    $newAssemblyName = $prop.Value + "_" + (date -Format "yyyyMMddHHmmss")
+
+    $prop.Value = $newAssemblyName
+
+    [pscustomobject]@{
+        NewName = $newAssemblyName;
+        OldName = $originalAssemblyName;
+        Project = $proj
+    }
+}
+
+# TODO extract output path from
+# $cm.ActiveConfiguration.Properties.Item("OutputPath").Value
+function Build-TypeProvider {
+    $projects = "FsAsn1", "FsAsn1.TypeProvider"
+
+    $assemblyChanges = $projects | % { Set-UniqueAssemblyName $_ }
+
+    $references = ($assemblyChanges | %{ "#r @`"..\..\src\FsAsn1.TypeProvider\bin\Debug\$($_.NewName).dll`"" }) -join "`n"
+
+    $DTE.Solution.Projects `
+    | % projectitems `
+    | ? name -eq "load-tp-assemblies.fsx" `
+    | % FileNames(0) `
+    | % {[System.IO.File]::WriteAllText($_, "#I __SOURCE_DIRECTORY__`n$references")}
+
+
+    $c = (Get-Project "FsAsn1.TypeProvider").ConfigurationManager.ActiveConfiguration
+    $originalValue = $c.Properties.Item("DefineConstants").Value
+    $c.Properties.Item("DefineConstants").Value = $originalValue + ";TYPE_PROVIDER_ASSEMBLY"
+
+    $waitForBuildToFinish = $true
+    $DTE.Solution.SolutionBuild.Build($waitForBuildToFinish)
+
+
+    $c.Properties.Item("DefineConstants").Value = $originalValue
+
+    $assemblyChanges | % { $_.Project.Properties.Item("AssemblyName").Value = $_.OldName }
+
+    $f = Get-Project "FsAsn1.Tests" | % projectitems | ? name -eq "Script1.fsx"
+    $DTE.ItemOperations.OpenFile($f.FileNames(0)) | Out-Null
+}
+
+Build-TypeProvider
