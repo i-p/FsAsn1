f7d456bfbaf5b4104021a2936942636737b91a5f
i-p <ivan12@gmail.com>
2016-08-04 20:39:27 +0200

    Add load scripts for F# interactive

---
+++ src/FsAsn1/Scripts/load-references-release.fsx
@@ -0,0 +1,7 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../../packages/FParsec-Big-Data-Edition/lib/net45/FParsec.dll"
+#r "../../../packages/FParsec-Big-Data-Edition/lib/net45/FParsecCS.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
