32e3579d130546dde9c23687cd37103cdb4d8779
i-p <ivan12@gmail.com>
2016-08-07 10:10:21 +0200

    Add FsAsn1.Viewer project

---
+++ src/FsAsn1.Viewer/AssemblyInfo.fs
@@ -0,0 +1,11 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsAsn1.Viewer")>]
+[<assembly: AssemblyProductAttribute("FsAsn1.Viewer")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
