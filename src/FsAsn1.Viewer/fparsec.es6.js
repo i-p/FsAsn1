50c3ed23a3b2772d26478b914cec050a774f94cc
i-p <ivan12@gmail.com>
2016-08-07 15:24:30 +0200

    Add JS module fparsec.es6.js that maps functionality of FParsec to bennu library

---
+++ src/FsAsn1.Viewer/fparsec.es6.js
@@ -0,0 +1,121 @@
+
+import {parse, text, lang} from "bennu"
+import {stream} from "nu-stream"
+import {List} from "fable-core"
+
+//TODO class
+function LastConsumedPosition(index, line, column, previous) {
+    this.index = index;
+    this.line = line;
+    this.column = column;
+    this.previous = previous;
+}
+
+LastConsumedPosition.initial = new LastConsumedPosition(0, 1, 1, null);
+
+LastConsumedPosition.prototype.toString = function() { return `Line: ${this.line} Column: ${this.column}` }; 
+
+LastConsumedPosition.prototype.increment = function(tok, restOfInput) {
+    let isNewLine = tok === "\n" || (tok === "\r" && (!restOfInput || restOfInput.first !== "\n"));
+
+    if (isNewLine) {
+        return new LastConsumedPosition(this.index + 1, this.line + 1, 1, tok); 
+    } else {
+        return new LastConsumedPosition(this.index + 1, this.line, this.column + 1, tok); 
+    }    
+};
+    
+LastConsumedPosition.prototype.eq = function(other) {
+    return other && other.index === this.index && other.previous === this.previous; 
+};
+
+let eagerList = function (p) {
+	return parse.bind(parse.eager(p), arr => parse.of(List.ofArray(arr)));
+}
+		
+let manyString = (p) => parse.bind(parse.eager(parse.many(p)), arr => parse.of(arr.join("")));
+let many1String = (p) => parse.bind(parse.eager(parse.many1(p)), arr => parse.of(arr.join("")));
+		
+
+let newline = parse.choice(text.string("\n"), text.string("\r\n"), text.string("\r"));
+
+let wrapInSuccess = (p) =>
+    parse.bind(p, 
+        r => parse.bind(parse.getParserState, 
+            s => parse.of({ Case: 'Success', Fields: [r, s.userState, s.position]})))
+
+
+let choice = ps =>
+    parse.choicea(Array.from(ps))
+
+export default {
+    eof: parse.eof,
+	fail: (msg) => parse.never(msg),
+    attempt: p => parse.attempt(p),
+    choice,
+    skipManyTill: (p, endp) => parse.sequence(parse.eager(parse.manyTill(p, endp)), endp, parse.of(null)),
+    //TODO false not supported
+    skipRestOfLine: (skipNewline) => parse.sequence(parse.eager(parse.manyTill(parse.anyToken, newline)), newline),
+    manyTill: (p, endp) => lang.then(parse.eager(parse.manyTill(p, endp)), endp),
+	op_GreaterGreaterEquals: (p, f) => parse.bind(p, f),
+	anyChar: parse.anyToken,
+	skipString: (s) => parse.next(text.string(s), parse.of(null)),	
+    skipNewline: parse.next(parse.choice(text.string("\n"), text.string("\r\n"), text.string("\r")), parse.of(null)),
+    newline,
+    getPosition: parse.getPosition,
+    getUserState: parse.getState,
+	//TODO check this one
+	op_DotGreaterGreaterQmark: (p1, p2) =>
+		parse.attempt(lang.then(p1, p2)),
+    run: (p, str) => { 
+        try {    
+            return parse.runState(wrapInSuccess(p), new parse.ParserState(stream.from(str), LastConsumedPosition.initial));             
+        } catch (ex) {
+            return { Case: 'Failure', Fields: [ex.toString()] };
+        }	    
+	},
+    runParserOnSubstring: (p, userState, streamName, str, index, count) => { 
+        try {
+            return parse.runState(wrapInSuccess(p), new parse.ParserState(stream.from(str.substring(index, index + count)), LastConsumedPosition.initial, userState)); 
+        } catch (ex) {
+            return { Case: 'Failure', Fields: [ex.toString()] };
+        }	    
+    },
+    runParserOnString: (p, userState, streamName, str) => { 
+        try {
+            return parse.runState(wrapInSuccess(p), new parse.ParserState(stream.from(str), LastConsumedPosition.initial, userState));             
+        } catch (ex) {
+            return { Case: 'Failure', Fields: [ex.toString()] };
+        }	    
+    },
+	pstring: (str) => text.string(str),
+	pint32:
+		parse.bind(many1String(text.digit), n => parse.of(Number.parseInt(n))),
+	spaces:
+		manyString(text.space),
+	numberLiteral: (opts, label) =>
+		parse.bind(many1String(text.digit), s => parse.of({ string: s })),
+	opt: p => parse.optional(null, p),
+	many: p => eagerList(parse.many(p)),
+	sepBy: (p, sep) => eagerList(lang.sepBy(sep, p)),
+	op_BarGreaterGreater: (p, f) => parse.bind(p, x => parse.of(f(x))),
+	op_LessBarGreater: parse.either,
+	op_LessBarGreaterPercent: (p, v) => parse.either(p, parse.of(v)),
+	op_GreaterGreaterDot:  parse.next,
+	op_DotGreaterGreater:  lang.then,
+	op_DotGreaterGreaterDot: (p1, p2) => parse.bind(p1, x => parse.bind(p2, y => parse.of([x,y]))),
+	op_GreaterGreaterPercent: (p1, v) => parse.next(p1, parse.of(v)),
+	between: lang.between,
+	pipe3: (p1,p2,p3,f) =>
+		parse.bind(p1, 
+			x => parse.bind(p2, 
+				y => parse.bind(p3, 
+					z => parse.of(f(x)(y)(z))))),
+	pipe2: (p1, p2, f) =>
+		parse.bind(p1, x => parse.bind(p2, y => parse.of(f(x)(y)))),
+	createParserForwardedToRef: () =>
+		{ let ref = {contents: undefined}; return [parse.late(() => ref.contents), ref]},
+	//TODO fix
+	many1Satisfy2L: (p1, p2, p3) =>
+		many1String(parse.token(x => /[0-9a-zA-Z-]/.test(x)))
+};
