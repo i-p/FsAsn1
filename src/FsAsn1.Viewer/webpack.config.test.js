2c26856c947dabb148c19e56d9ff4a9cd6890c3a
i-p <ivan12@gmail.com>
2017-03-23 22:13:23 +0100

    Add polyfill for TextDecoder when running tests
    This required newer version of webpack that supports property exports in ProvidePlugin.

--- src/FsAsn1.Viewer/webpack.config.test.js
+++ src/FsAsn1.Viewer/webpack.config.test.js
@@ -2,14 +2,20 @@ var path = require("path");
 var webpack = require("webpack");
 var nodeExternals = require('webpack-node-externals');
 
+// WORKAROUND https://github.com/webpack/webpack/issues/4530
+function capitalizeDriveLetter(path) {
+    return path.charAt(0).toUpperCase() + path.slice(1);
+}
+
 var cfg = {
   devtool: "source-map",
   target: "node",
   entry: ["./ParserTests.js", "./FParsecTests.js", "./ReaderTests.js"],
   output: {
-    path: __dirname, //path.join(__dirname, "public"),
+    path: capitalizeDriveLetter(path.resolve(__dirname)),
     filename: "tests.bundle.js"
   },
+  context: capitalizeDriveLetter(path.resolve(__dirname)),
   module: {
     rules: [
       {
@@ -26,7 +32,10 @@ var cfg = {
   },
   externals: [nodeExternals({
 	  whitelist: [/^fable-core/]
-  })]
+  })],
+  plugins: [
+      new webpack.ProvidePlugin({ TextDecoder: ['text-encoding', 'TextDecoder'] })
+  ]
 };
 
 module.exports = cfg;
