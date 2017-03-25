6f4b7f42f81b5dd5d421f508836bf6bf499c31d2
i-p <ivan12@gmail.com>
2017-03-25 11:48:58 +0100

    Fix webpack config

--- src/FsAsn1.Viewer/webpack.config.js
+++ src/FsAsn1.Viewer/webpack.config.js
@@ -1,13 +1,19 @@
 var path = require("path");
 var webpack = require("webpack");
 
+// WORKAROUND https://github.com/webpack/webpack/issues/4530
+function capitalizeDriveLetter(path) {
+    return path.charAt(0).toUpperCase() + path.slice(1);
+}
+
 var cfg = {
   devtool: "source-map",
   entry: "./Viewer.js",
   output: {
-    path: path.join(__dirname, "public"),
+    path: capitalizeDriveLetter(path.join(__dirname, "public")),
     filename: "bundle.js"
   },
+  context: capitalizeDriveLetter(path.resolve(__dirname)),
   module: {
     rules: [
       {
